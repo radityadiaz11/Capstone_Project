585662cd40051b84af57dddc9eaa028c5c71bd17
fadhilahsalsa-06 <fadilahsalsa61@gmail.com>
2026-05-22 13:57:05 +0700

    habis bikin role ortu, tolong di cek

--- traking-snbp/src/App.jsx
+++ traking-snbp/src/App.jsx
@@ -1,14 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import DashboardPage from './pages/DashboardPage';
-import PrediksiSiswa_Page from './pages/PrediksiSiswa_Page';
-import DetailSiswa_Page from './pages/DetailSiswa_Page';
-import DataNilai_Page from './pages/DataNilai_Page';
-import MonitoringKelas_Page from './pages/MonitoringKelas_Page';
-import StatistikSNBP_Page from './pages/StatistikSNBP_Page';
-import EksporData_Page from './pages/EksporData_Page';
-import Notifikasi_Page from './pages/Notifikasi_Page';
-import Pengaturan_Page from './pages/Pengaturan_Page';
+import LoginPage from './pages/auth/LoginPage';
+import DashboardPage from './pages/guru/DashboardPage';
+import PrediksiSiswa_Page from './pages/guru/PrediksiSiswa_Page';
+import DetailSiswa_Page from './pages/guru/DetailSiswa_Page';
+import DataNilai_Page from './pages/guru/DataNilai_Page';
+import MonitoringKelas_Page from './pages/guru/MonitoringKelas_Page';
+import StatistikSNBP_Page from './pages/guru/StatistikSNBP_Page';
+import EksporData_Page from './pages/guru/EksporData_Page';
+import Notifikasi_Page from './pages/guru/Notifikasi_Page';
+import Pengaturan_Page from './pages/guru/Pengaturan_Page';
+import DashboardOrtu_Page from './pages/ortu/DashboardOrtu_Page';
+import NilaiRapor_Page from './pages/ortu/NilaiRapor_Page';
+import PrediksiSnbp_Page from './pages/ortu/PrediksiSnbp_Page';
+import PesanGuru_Page from './pages/ortu/PesanGuru_Page';
+
 
 function App() {
   return (
@@ -23,9 +28,15 @@ function App() {
       <Route path="/ekspor-data" element={<EksporData_Page />} />
       <Route path="/notifikasi" element={<Notifikasi_Page />} />
       <Route path="/pengaturan" element={<Pengaturan_Page />} />
+      <Route path="/ortu/dashboard" element={<DashboardOrtu_Page />} />
+      <Route path="/ortu/nilai" element={<NilaiRapor_Page />} />
+      <Route path="/ortu/prediksi-snbp" element={<PrediksiSnbp_Page />} />
+      <Route path="/ortu/pesan-guru" element={<PesanGuru_Page />} />
+
     </Routes>
   );
 }
 
 export default App
+
 
