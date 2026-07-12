2a827b88bad8bd89f8d1e6f7638c26881ff804de
radityadiaz11 <radityadiaz11@gmail.com>
2026-07-13 03:00:48 +0700

    Menghapus folder traking-snbp dan snbpredict-backend yang lama

--- snbpredict-backend/src/routes/academicRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const academicController = require('../controllers/academicController');
-
-router.get('/scores', academicController.getScores);
-router.get('/monitoring', academicController.getMonitoring);
-
-module.exports = router;
--- snbpredict-backend/src/routes/predictRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const { predictStudent } = require('../controllers/predictController');
-const { protect }        = require('../middleware/authMiddleware');
-
-// POST /api/v1/predict → prediksi performa siswa
-router.post('/', protect, predictStudent);
-
-module.exports = router;
